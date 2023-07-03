79c13343ad87b2c18f0d701819790414f56e475e
Andrew Paull <109480468+wakedreamer@users.noreply.github.com>
2023-07-04 08:46:21 +1000

    refactor: remove unnecessary comment

--- app/api/entities/feedback_entities/stage_entity.rb
+++ app/api/entities/feedback_entities/stage_entity.rb
@@ -1,7 +1,7 @@
 module Entities
   class StageEntity < Grape::Entity
     expose :id
-    expose :title # expose method: returns title of stage from stage.rb
+    expose :title
     expose :order
     expose :entry_message
     expose :exit_message_good
