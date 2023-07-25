f93f2262b0e21850794b7d2b26f961fb5cd4040c
Andrew Paull <109480468+wakedreamer@users.noreply.github.com>
2023-07-25 18:15:53 +1000

    fix: fix associations

--- test/factories/feedback/feedback_comment_template_factory.rb
+++ test/factories/feedback/feedback_comment_template_factory.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
     factory :feedback_comment_template do
-      criterion_option
   
       comment_text_situation    { Faker::Lorem.sentence }
       comment_text_next_action  { Faker::Lorem.sentence }
