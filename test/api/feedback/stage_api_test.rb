4bb9c0aa39bba507873647c991c8c85d9eda054a
Andrew Paull <109480468+wakedreamer@users.noreply.github.com>
2023-07-25 18:07:03 +1000

    quality: "setup" tests to avoid repeating code

--- test/api/feedback/stage_api_test.rb
+++ test/api/feedback/stage_api_test.rb
@@ -10,73 +10,44 @@ class StageApiTest < ActiveSupport::TestCase
     Rails.application
   end
 
-  # Test: a main convenor can POST a stage
-  def test_create_stage_via_post
-    unit = FactoryBot.create(:unit)
-
-    data_to_post = {
+  setup do
+    @unit = FactoryBot.create(:unit)
+    @data_to_post = {
       title: 'Stage 1',
       order: '1',
-      task_definition_id: unit.task_definitions.first.id
+      task_definition_id: @unit.task_definitions.first.id
     }
-
-    user = unit.main_convenor_user
-
-    stage_count = Stage.count
-
-    # expected_unit = data_to_post[:unit]
-    # unit_count = Unit.all.length
-
-    # Add username and auth_token to Header. 
-    # Header: a key-value pair sent with the request to the server so that the server can identify the user.
-    # The header is the part of the request that contains the username and auth_token.
-    add_auth_header_for(user: user)
-
-    # The post that we will be testing.
-    post_json '/api/stages', data_to_post
-
-    # Check that the unit name matches what is expected
-    assert_equal 201, last_response.status, last_response.body # TTP status code 201 = created
-
-    # Read the new stage from the database
-    stage = Stage.last
-
-    # Check that the stage title matches what is expected
-    assert_equal data_to_post[:title], stage.title
-    # Check that the stage order matches what is expected
-    assert_equal data_to_post[:order].to_i, stage.order
-    # Check that the stage count has increased by 1
-    assert_equal stage_count + 1, Stage.count
-
-    # Delete the unit created for this test from the database
-    unit.destroy
   end
 
-  # Test: a student cannot POST a stage
-  def test_students_cannot_create_stages
-    unit = FactoryBot.create(:unit)
+  # Test that a main convenor of the unit can POST a stage
+  def test_create_stage_via_post
 
-    data_to_post = {
-      title: 'Stage 1',
-      order: '1',
-      task_definition_id: unit.task_definitions.first.id
-    }
-
-    user = unit.students.first.user
+    user = @unit.main_convenor_user # adds username and auth_token to Header part of request to the server
+    add_auth_header_for(user: user)
 
     stage_count = Stage.count
 
-    # Add username and auth_token to Header
+    post_json '/api/stages', @data_to_post # the post we will be testing
+    assert_equal 201, last_response.status, last_response.body # HTTP status code 201 = created
+
+    stage = Stage.last # reads the new stage from the database
+
+    assert_equal @data_to_post[:title], stage.title # checks that the stage title matches what is expected
+    assert_equal @data_to_post[:order].to_i, stage.order # checks the stage order matches what is expected
+    assert_equal stage_count + 1, Stage.count # checks the stage count has increased by 1
+  end
+
+  # Test that a student of the unit cannot POST a stage
+  def test_students_cannot_create_stages
+
+    user = @unit.students.first.user
+
+    stage_count = Stage.count
     add_auth_header_for(user: user) # the API request is sent with the student's username and auth_token, to check if the student is allowed to do this
 
-    # The post that we will be testing.
-    post_json '/api/stages', data_to_post
+    post_json '/api/stages', @data_to_post
 
-    # Check to see if the unit's name matches what was expected
-    assert_equal 403, last_response.status, last_response.body
-
+    assert_equal 403, last_response.status, last_response.body # 403 = Forbidden, indicates that the server understands the request but can't provide additional access
     assert_equal stage_count, Stage.count
-
-    unit.destroy
   end
 end
